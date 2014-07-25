db08b1a1b3b0c694298ebba1a7fcf7400200dcb0
Alex Indigo <iam@alexindigo.com>
2014-07-25 16:51:50 -0700

    Auto update dependencies

--- start.sh
+++ start.sh
@@ -5,14 +5,17 @@ CACHE_HOST="${REDIS_PORT_6379_TCP_ADDR}"
 CACHE_PORT="${REDIS_PORT_6379_TCP_PORT}"
 
 # make config up to date
-if [ -f "/www/_browsenpm.json" ]
+if [ -f "_browsenpm.json" ]
 then
-  sed 's%REGISTRY_HOST%'${REGISTRY_HOST}'%' /www/_browsenpm.json > /www/_browsenpm.tmp1.json
-  sed 's%REGISTRY_PORT%'${REGISTRY_PORT}'%' /www/_browsenpm.tmp1.json > /www/_browsenpm.tmp2.json
-  sed 's%CACHE_HOST%'${CACHE_HOST}'%' /www/_browsenpm.tmp2.json > /www/_browsenpm.tmp1.json
-  sed 's%CACHE_PORT%'${CACHE_PORT}'%' /www/_browsenpm.tmp1.json > /www/_npm_ui.json
-  rm /www/_browsenpm.tmp1.json /www/_browsenpm.tmp2.json
+  sed 's%REGISTRY_HOST%'${REGISTRY_HOST}'%' _browsenpm.json > _browsenpm.tmp1.json
+  sed 's%REGISTRY_PORT%'${REGISTRY_PORT}'%' _browsenpm.tmp1.json > _browsenpm.tmp2.json
+  sed 's%CACHE_HOST%'${CACHE_HOST}'%' _browsenpm.tmp2.json > _browsenpm.tmp1.json
+  sed 's%CACHE_PORT%'${CACHE_PORT}'%' _browsenpm.tmp1.json > _npm_ui.json
+  rm _browsenpm.tmp1.json _browsenpm.tmp2.json
 fi
 
+# update dependencies
+npm install
+
 # Start browsenpm
-/www/bin/server -c /www/_npm_ui.json
+bin/server -c _npm_ui.json
